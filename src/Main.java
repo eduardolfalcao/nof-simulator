f84ec3932d05b7f8f14c71ce629cff9c10951b7e
eduardolfalcao <eduardolfalcao@lsd.ufcg.edu.br>
2014-10-10 17:24:51 -0300

    With a script in main to calculate PI and D for different Ks.

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,66 @@ import simulator.Simulator;
 
 public class Main {
 	
-	public final static int replications = 1;
+	public final static int replications = 2;
 	
 	public static void main(String [] args){
 		
+		double [] d = new double[15];
+		
+		double initialDemand = 2;
+		double finalDemand = 4;
+		double delta = ((finalDemand - initialDemand)/14);
+		d[0] = initialDemand;
+		for(int i = 1; i <= 14; i++)
+			d[i] = (double)(initialDemand+(i*delta));
+		
+		double [] kappa = {0.5, 1, 2, 4};
+		double [][] pi = new double[15][4];
+		
+		for(int i = 0; i<15; i++){
+			for(int j = 0; j<4; j++)
+				pi[i][j]= (kappa[j]/(d[i]-1+kappa[j]));
+		}
+		
+		for(int i =0; i < 15; i++)
+			System.out.println((d[i]+"").replace(".", ","));
+		
+		for(int i =0; i < 15; i++){
+			System.out.println("PI["+i+"]");
+			for(int j = 0; j<4; j++)
+				System.out.println((pi[i][j]+"").replace(".", ","));
+		}
+		
+		double [] probability = new double[15];
+		double initialProbability = 0.2;
+		double finalProbability = 0.8;
+		delta = ((finalProbability - initialProbability)/14);
+		probability[0] = initialProbability;		
+		for(int i = 1; i <= 14; i++)
+			probability[i] = (double)(initialProbability+(i*delta));
+		
+		System.out.println("Probability");
+		for(int i =0; i < 15; i++)
+			System.out.println((probability[i]+"").replace(".", ","));
+		
+		double [][] demand = new double[15][4];
+		
+		for(int i = 0; i<15; i++){
+			for(int j = 0; j<4; j++)
+				demand[i][j]= (((kappa[j]-(kappa[j]*probability[i]))/probability[i])+1);
+		}
+		
+		for(int i =0; i < 15; i++){
+			System.out.println("D["+i+"]");
+			for(int j = 0; j<4; j++)
+				System.out.println((demand[i][j]+"").replace(".", ","));
+		}
+		
+		System.exit(0);
+		
 		long startTime = System.currentTimeMillis();
 		
-		int replicationIndex = 1;
+		int replicationIndex = 2;
 		double capacitySupplied = 1;
 		double changingValue = 0.05;		
 		boolean nofWithLog = false;			//with sqrt
@@ -22,9 +75,9 @@ public class Main {
 		int [] numPeersSimulations = {500};									//100
 		int [] numStepsSimulations = {4000};								//2000
 		boolean [] dynamicSimulations = {false};						//{true,false};
-		double [] percentageCollaboratorsSimulations = {0.25,0.5,0.75};			//0.2, 0.75
-		double [] consumingStateProbabilitySimulations = {0.5};		//0.2, 0.5
-		double [] peersDemandSimulations = {2};								//2
+		double [] percentageCollaboratorsSimulations = {0.25, 0.5, 0.75};			//0.2, 0.75
+		double [] consumingStateProbabilitySimulations = {0.5};						//0.335, 0.5, 0.667, 0.8
+		double [] peersDemandSimulations = {1.5, 2, 3, 5};							//1.5, 2, 3, 5
 		
 		
 		
@@ -37,15 +90,18 @@ public class Main {
 						for(double consumingStateProbability : consumingStateProbabilitySimulations){
 							for(double percentageCollaborators : percentageCollaboratorsSimulations){
 								for(double peersDemand : peersDemandSimulations){
-										String path = "/home/eduardolfalcao/Área de Trabalho/Dropbox/Doutorado/Disciplinas/Projeto de Tese/CCGRID 2015/preliminares/k colaboradores/";
+										String path = "/home/eduardolfalcao/Área de Trabalho/Dropbox/Doutorado/Disciplinas/Projeto de Tese/CCGRID 2015/preliminares/baseline estático/varia D/";
 										
-										file = "Dynamic  ("+dynamic+") - "
-												+numPeers+" peers - "
-												+numSteps+" steps - "
-												+((1-percentageCollaborators)*100)+"% freeriders - "+(consumingStateProbability*100)+"% consuming probability - "
-												+peersDemand+" peers demand - "
-												+(changingValue*100)+"% change value - "
-												+ "NoF by "+(nofWithLog==false?"SquareRoot":"Log")
+										double k = ((consumingStateProbability*(peersDemand-1))/(1-consumingStateProbability));
+										
+										file =  "k-"+(String.format("%.1f", k).replace(",", "."))+" "
+												+"C-"+(dynamic==true?"dynamic":"static")+" "
+												+"n-"+numPeers+" "
+												+"fr-"+((int) ((1-percentageCollaborators)*100))+" "
+												+"pi-"+(String.format("%.1f", consumingStateProbability*100)).replace(",", ".")+" "
+												+"D-"+peersDemand
+												//+(changingValue*100)+"% change value - "
+												//+ "NoF by "+(nofWithLog==false?"SquareRoot":"Log")
 												+ ".xlsx";
 										Simulator s1 = new Simulator(numPeers, numSteps, consumingStateProbability, percentageCollaborators, dynamic, nofWithLog, peersDemand, capacitySupplied, changingValue, replicationIndex, Level.SEVERE, path+replicationIndex+"/"+file);
 										s1.startSimulation();
